8939addd92580dfdd1118255e5e0d5d7db01e9b1
FEBAANISH <42109037+FEBAANISH@users.noreply.github.com>
2018-08-05 02:28:41 -0700

    Update GitClass.java

--- SampleGit/src/GitPackage/GitClass.java
+++ SampleGit/src/GitPackage/GitClass.java
@@ -5,6 +5,7 @@ public class GitClass {
 	public static void main(String[] args) {
 		System.out.println("GitHub Testing");
 		System.out.println("Pushing code");
+		System.out.println("Pulling code");
 	}
 
 }
